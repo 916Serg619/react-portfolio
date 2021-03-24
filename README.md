936600a7e7d1a683c052f6903a5193dcd750088a
Sergio Aguirre <sergioaguirre@sergios-mbp.attlocal.net>
2021-03-23 17:52:30 -0700

    updated REadME

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Create portfolio using React app
 
 Refactor Portfolio in to a React app
 
-## Challenges
+## Challenges/Successes
 
-I'm having issues in three major areas: My images are not loading, my scroll function is not fully implemented just yet, and my css has to be manipulated further. The real struggle came in breaking down my components to mimic my old portfolio to the T. Unfortunately, I'm going to have to make a few comprimises, in order to get this working effectively. Definitely not finished, will be very soon.
+Currently, my app is deployed to GH Pages, I will be adding more scripts, projects, and fron end work soon.
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,9 @@
 import React  from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
+import profilePic from '../Assets/Profile-pic.jpg';
+import Resume from '../Assets/SergioAguirre.Resume.UCSD.pdf';
+
 
 
 
@@ -27,6 +30,42 @@ const Navbar = () => {
                             </li>
                         </ul>
                 </div>
+                <div className="main-info">
+            <section className="about" id="about">
+        <div className="col-6">
+            <h2 className="title" id='about-me'>About Me</h2>
+            <div className="about-content">
+                <div className="column-left profilePic">
+                    <img src={profilePic} className='profilePic' alt="Sergio Aguirre"></img>
+                </div>
+                <div className="column-right">
+                    <p>Hello! My name is Sergio Aguirre. With a background in the Audio/Visual field, paired with my current knowledge of web development, my goal is to help you solidify your brand, company, or product, to be ready for the modern world.</p>
+                    <a href={Resume}>Download Resumé</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section className="services" id="services">
+        <div className="max-width">
+            <h2 className="title">My Services</h2>
+            <div className="service-content">
+                <div className="card">
+                    <div className="box">
+                        <div className="text">Web Design</div>
+                        <p>Need a website? Want a personalized app? I am here for all your development needs.</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="box">
+                        <div className="text">Audio/Visual</div>
+                        <p>With years of experience with multiple audio and visual programs, I am here to help make your vision come to life!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+ </div>
               </nav>
             
     )
